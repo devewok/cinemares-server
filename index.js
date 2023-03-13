c6d40429310af7df374aaac98c9623dde7507fa7
bsjaramillo <bsjaramillo1995@gmail.com>
2023-03-12 21:18:44 -0500

    added cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
 	socket.on('moviechat', message => {
